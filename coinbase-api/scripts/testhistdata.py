1b272792f8d582e53f5e923d977e715ddab774e2
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2019-11-13 21:13:53 -0500

    wip

--- coinbase-api/scripts/testhistdata.py
+++ coinbase-api/scripts/testhistdata.py
@@ -3,25 +3,28 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 import time
+import requests
 
 # initialize coinbase pro client with public functions
-pc = cbpro.PublicClient()
 product_id = 'BTC-USD'
 
 # set end to current time and iterate backwards to specified start
 start = dt.datetime(2019, 8, 1)
 end = dt.datetime.now()
+print(type(end))
 
 hist_data = []
 
 # while end != start:
 for x in [1,2,3]:
+    pc = cbpro.PublicClient()
     print(end)
-    new_end = end
-    print(new_end)
-    new_data = pc.get_product_historic_rates(product_id, end=new_end, granularity=86400)
+    param_end = end.ToString('yyyy-mm-dd')
+    print(param_end)
+    new_data = pc.get_product_historic_rates(product_id, end=param_end, granularity=86400)
     hist_data.extend(new_data)
     end = dt.datetime.fromtimestamp(new_data[-1][0])
     print(end)
     print('')
+    print(pc.session)
     time.sleep(5)
