c1fdfe10d518fbbf5bbe23f45ae2cc31782179b7
ericbrown <ericbrown1.2018@u.northwestern.edu>
2019-11-23 16:31:25 -0500

    Store Websocket information in DataFrame

---
+++ EsJupyter/websocket.py
@@ -0,0 +1,28 @@
+import asyncio
+import pandas as pd
+import json
+from copra.websocket import Channel, Client
+from autobahn.asyncio.websocket import WebSocketClientFactory
+
+loop = asyncio.get_event_loop()
+global dataFeed
+
+class MyClient(Client):
+
+    def on_message(self, message):
+        dataFeed = pd.DataFrame(message, index=[len(message)])
+        print(dataFeed)
+
+
+# ws = MyClient(loop, Channel('heartbeat', 'BTC-USD'))
+ws1 = MyClient(loop, Channel('ticker', 'BTC-USD'))
+
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    # loop.run_until_complete(ws.close())
+    loop.run_until_complete(ws1.close())
+    loop.close()
+
+
